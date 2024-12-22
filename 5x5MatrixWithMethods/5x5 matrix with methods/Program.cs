15c91f028c25bcabbf70de0e61cad289595d19fa
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2024-12-22 17:12:43 +0200

    Add files via upload

---
+++ 5x5MatrixWithMethods/5x5 matrix with methods/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5x5_matrix_with_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[5, 5];
+            Random random = new Random();
+            FillMatrix(matrix, random);
+            PrintResult(matrix);
+        }
+        static void PrintResult(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 1)
+                    {
+                        Console.Write($"{'@'} ");
+                    }
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void FillMatrix(int[,] matrix, Random random)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(1, 51);
+                }
+            }
+        }
+    }
+}
