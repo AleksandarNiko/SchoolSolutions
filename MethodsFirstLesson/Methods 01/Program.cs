e6d8c43b409fe1293d3a1e2d76109c8df0e2b1a1
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2024-12-23 09:45:36 +0200

    Add files via upload

---
+++ MethodsFirstLesson/Methods 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDigits();
+        }
+
+        private static void PrintDigits()
+        {
+            int a = 10;
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ sedlovaTocka/sedlovaTocka/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sedlovaTocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 5]; 
+            Random random = new Random();
+
+            //print matrix
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(10, 100);
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            //min numbers in cols
+            List<int> mins = new List<int>();
+            Console.WriteLine("min numbers in cols:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int min = matrix[0, i];
+                for (int j = 1; j < matrix.GetLength(0); j++)
+                    if (matrix[j, i] < min)
+                        min = matrix[j, i];
+                mins.Add(min);
+                
+            }
+            Console.WriteLine(string.Join(" ", mins));
+
+            //max numbers in cols
+            int[] maxes = new int[matrix.GetLength(1)];
+            Console.WriteLine("max numbers in cols:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int max = int.MinValue;
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
+                }
+                maxes[i] = max;
+            }
+            Console.WriteLine(string.Join(" ", maxes));
+        }
+    }
+}
