d02032a52078fb2eeb67a138c56c96e8db8281bd
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-01-05 21:56:43 +0200

    Add files via upload

---
+++ zad25/zad25/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+
+double discriminant =Math.Sqrt((a * a) - (4 * a * b));
+
+if (discriminant < 0)
+{  
+    Console.WriteLine("Няма реални решения.");
+}
+else if(discriminant > 0)
+{
+    double x1 = -sqrtDiscriminant;
+    double x2 = sqrtDiscriminant;
+
+    Console.WriteLine( $"Решенията са ({x1}; {x2}). ");
+}
+else
+{
+    Console.WriteLine();
+}
