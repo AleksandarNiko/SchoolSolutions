75a6e9f2ec63667fced244b972e6a901338ced7e
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-01-09 21:20:00 +0200

    Add files via upload

---
+++ schoolWork/05.average/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _05.average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n=double.Parse(Console.ReadLine());
+            double average = 0;
+            double sum = 0;
+            int count = 0;
+            while(n<=100 && n%7==0)
+            {            
+                sum += n;
+                count++;
+                average =sum/count;              
+            }
+            Console.WriteLine(average);
+        }
+    }
+}
---
+++ schoolWork/NOD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            while (b != 0)
+            {
+                int maxB = b;
+                b = a % b;
+                a = maxB;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
