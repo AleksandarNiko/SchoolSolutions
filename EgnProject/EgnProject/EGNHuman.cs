70220faab743a35412df2e278fe0522409c3c4c3
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-01-14 20:42:42 +0200

    Add files via upload

---
+++ EgnProject/EgnProject/EGNHuman.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class EGNHuman
+{
+    private string eGN;
+
+    public string EGN { get => eGN; set 
+       {
+            if (value.Length != 10)
+            {
+                throw new ArgumentException("ЕГН трябва да съдържа точно 10 цифри.");
+            }
+             eGN = value;
+        }
+    }
+
+    public EGNHuman(string egn)
+    {
+       EGN= egn;
+    }
+
+    public bool IsValid()
+    {
+        if (EGN.Length != 10) 
+        {
+            return false;
+        }
+
+        int[] weights = { 2, 4, 8, 5, 10, 9, 7, 3, 6 };
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++)
+        {
+            var currDigit = int.Parse(EGN[i].ToString());
+            sum += currDigit * weights[i];
+        }
+
+        int controlDigit = sum % 11;
+        if (controlDigit == 10) 
+        {
+            controlDigit = 0; 
+        }
+
+        return (controlDigit == int.Parse(EGN[9].ToString())) ? true : false;
+    }
+
+    public string GetGender()
+    {
+        int genderDigit = int.Parse(EGN[8].ToString());
+
+        return (genderDigit % 2 == 0) ? "Мъж" : "Жена";
+    }
+
+    public string GetBirthDate()
+    {
+        int year = int.Parse(EGN.Substring(0, 2));
+        int month = int.Parse(EGN.Substring(2, 2));
+        int day = int.Parse(EGN.Substring(4, 2));
+
+        if (month > 20)
+        {
+            month -= 40;
+            year += 2000;
+        }
+        else if (month > 0 && month <= 20)
+        {
+            month -= 20;
+            year += 1900;
+        }
+
+        var birthDate = new DateTime(year, month, day);
+
+        return birthDate.ToString("dd.MM.yyyy");
+    }
+}
---
+++ EgnProject/EgnProject/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EgnProject
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox2.Text;
+            string egn = textBox1.Text;
+            
+            EGNHuman egnHuman = new EGNHuman(egn);
+
+            if (!egnHuman.IsValid())
+            {
+                label1.Text = "Невалидно ЕГН!";
+                return;
+            }
+
+            string gender = egnHuman.GetGender();
+            string birthDate = egnHuman.GetBirthDate();
+
+            label1.Visible = true;
+
+            label1.Text = $"Име: {name}" +
+                $"\nЕГН: {egn}" +
+                $"\nПол: {gender}" +
+                $"\nДата на раждане: {birthDate}";
+
+            }
+        }
+    }
