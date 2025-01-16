b60a6bc4d034037f5536059a888e9caf2f55602c
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-01-16 22:14:22 +0200

    Add files via upload

---
+++ Generics/01.BoxProject/Box_All.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.BoxProject
+{
+    public class Box_All<T>
+    {
+        private List<T> _data;
+
+        public Box_All()
+        {
+            _data = new List<T>();
+        }
+
+        public void Add_box(T element)
+        {
+            _data.Add(element);
+        }
+
+        public T Remove_box() 
+        {
+            _data.Remove(_data.Last());
+            return _data.Last();
+        }
+
+        public  int Count { get; }
+
+        public void Display()
+        {
+            foreach (var item in _data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ Generics/01.BoxProject/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _01.BoxProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box_All<string> box = new Box_All<string>();
+
+            string choice = "";
+            while (choice != "exit")
+            {
+                Console.WriteLine("Enter your choice: ");
+                Console.WriteLine("1 - Add a treasure");
+                Console.WriteLine("2 - Add a candy");
+                Console.WriteLine("3 - Display the content of the box");
+                Console.WriteLine("4 - Display the number of elements in the box");
+                Console.WriteLine("D - Remove an element");
+                Console.WriteLine("Exit - Exit the program");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter a treasure: ");
+                        string treasure = Console.ReadLine();
+                        box.Add_box(treasure);
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter a candy: ");
+                        string candy = Console.ReadLine();
+                        box.Add_box(candy);
+                        break;
+                    case "3":
+                        Console.WriteLine("The content of the box is: ");
+                        box.Display();
+                        break;
+                    case "4":
+                        Console.WriteLine("The number of elements in the box is: ");
+                        Console.WriteLine(box.Count);
+                        break;
+                    case "D":
+                        Console.WriteLine("The last element was removed: ");
+                        Console.WriteLine(box.Remove_box());
+                        break;
+                    case "Exit":
+                        choice = "exit";
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
