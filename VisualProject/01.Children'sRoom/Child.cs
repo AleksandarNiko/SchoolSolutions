aa2bf3fd798e8a6d370d953fb378e718c7fec50c
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-01-30 22:06:37 +0200

    Add files via upload

---
+++ VisualProject/01.Children'sRoom/Child.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Children_sRoom
+{
+    public class Child
+    {
+        private string name;
+        private int age;
+
+        public Child(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Groups = new List<Group>();
+        }
+
+        public string Name { get => name; set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null!");
+                }
+                name = value;
+            } 
+        }
+
+        public int Age { get => age; set 
+            {
+                if (value < 10) 
+                {
+                    throw new ArgumentOutOfRangeException("Age cannot be less than 10!");
+                }
+                age = value;
+            } 
+        }
+
+        public  List<Group> Groups { get; set; }
+    }
+}
---
+++ VisualProject/01.Children'sRoom/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01.Children_sRoom
+{
+    public partial class Form1 : Form
+    {
+        private List<Teacher> teachers;
+        private List<Child> children;
+        private List<Group> groups;
+        public Form1()
+        {
+            InitializeComponent();
+            teachers = new List<Teacher>();
+            children = new List<Child>();
+            groups = new List<Group>();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GroupType groupType = new GroupType();
+
+            string groupTypeString= comboBox1.SelectedItem.ToString();
+            if (groupTypeString== "Млад художник")
+            {
+                groupType = GroupType.МХ;
+            }
+            else if (groupTypeString== "Изкуството в действие")
+            {
+                groupType = GroupType.ИВД;
+            }
+            else if (groupTypeString == "Млад програмист")
+            {
+                groupType = GroupType.МП;
+            }
+            else if (groupTypeString == "Аз програмирам игрите си")
+            {
+                groupType = GroupType.АПИ;
+            }
+            else if (groupTypeString == "Роботика")
+            {
+                groupType = GroupType.Р;
+            }
+            else if (groupTypeString == "Музиката е моя живот")
+            {
+                groupType = GroupType.ММЖ;
+            }
+
+            string day = comboBox2.SelectedItem.ToString();
+            string hour = comboBox3.SelectedItem.ToString();
+            string minutes = comboBox4.SelectedItem.ToString();
+            string groupTime = hour + ":" + minutes;
+
+            var group = new Group(groupType, day, groupTime);
+            groups.Add(group);
+
+            MessageBox.Show("Групата е добавена успешно!");
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name=textBox1.Text;
+            string specialty = textBox2.Text;
+            var teacher = new Teacher(name, specialty);
+            var groups=textBox3.Text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var group in groups)
+            {
+                var groupType = (GroupType)Enum.Parse(typeof(GroupType), group);
+                teacher.Groups.Add(new Group(groupType, "", ""));
+            }
+            teachers.Add(teacher);
+
+            MessageBox.Show("Преподавателят е добавен успешно!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string name= textBox4.Text;
+            int age = int.Parse(textBox5.Text);
+            var child = new Child(name, age);
+            var groups = textBox6.Text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var group in groups)
+            {
+                var groupType = (GroupType)Enum.Parse(typeof(GroupType), group);
+                child.Groups.Add(new Group(groupType, "", ""));
+            }
+
+            children.Add(child);
+
+            MessageBox.Show("Детето е добавено успешно!");
+        }
+    }
+}
+/* МХ,
+    ИВД,
+    МП,
+    АПИ,
+    Р,
+    ММЖ
+*/
