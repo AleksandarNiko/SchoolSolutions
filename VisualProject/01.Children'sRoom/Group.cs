92a32755991c38b8acefc827d6dd73949903fb37
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-02-04 21:04:50 +0200

    Add files via upload

---
+++ VisualProject/01.Children'sRoom/Group.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Children_sRoom
+{
+    public class Group
+    {
+
+        public GroupType Name { get; set; }
+
+        public string Day { get; set; }
+        public string Hour { get; set; }
+
+        public Group(GroupType name, string day, string hour)
+        {
+            Name = name;
+            Day = day;
+            Hour = hour;
+        }
+    }
+}
+
+public enum GroupType
+{
+    МХ,
+    ИВД,
+    МП,
+    АПИ,
+    Р,
+    ММЖ
+}
