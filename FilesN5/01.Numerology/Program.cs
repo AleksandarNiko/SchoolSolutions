73062a116256beb4f2b39ebf77a441e356c835a3
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-05-30 15:59:35 +0300

    Add files via upload

---
+++ FilesN5/01.Numerology/Program.cs
@@ -0,0 +1,86 @@
+﻿
+Dictionary<int, string> qualities = new Dictionary<int, string>()
+{
+    { 1, "Independence" },
+    { 2, "Diplomacy" },
+    { 3, "Natural talent" },
+    { 4, "Organizational skills" },
+    { 5, "Free spirit" },
+    { 6, "Caring and protection" },
+    { 7, "Philosophical skills" },
+    { 8, "Professionals" },
+    { 9, "Tolerance and humanity" },
+    { 11, "Visionaries with ideas" },
+    { 22, "Confidence and intuition" }
+};
+
+List<string> lines = ReadAndPrintOnConsole();
+
+Console.WriteLine();
+
+
+Dictionary<int, int> numbersCount = new Dictionary<int, int>();
+
+foreach (var line in lines)
+{
+    int personalNum = CalculatePersonalNumber(line);
+    Console.WriteLine($"{line} - лично число: {personalNum} ({qualities[personalNum]})");
+
+    if (numbersCount.ContainsKey(personalNum))
+    {
+        numbersCount[personalNum]++;
+    }
+    else
+    {
+        numbersCount[personalNum] = 1;
+    }
+}
+
+var mostCommon = numbersCount
+    .OrderByDescending(x => x.Value)
+    .ThenBy(x => x.Key)
+    .First();
+
+Console.WriteLine("\nНай-често срещано лично число:");
+Console.WriteLine($"{mostCommon.Key} - {qualities[mostCommon.Key]}");
+
+
+static int CalculatePersonalNumber(string birthDate)
+{
+    var parts = birthDate.Split('-');
+    int day = int.Parse(parts[0]);
+    int month = int.Parse(parts[1]);
+    int year = int.Parse(parts[2]);
+
+    int sum = day + month + year;
+
+    while (sum > 9 && sum != 11 && sum != 22)
+    {
+        sum = sum.ToString().Sum(c => c - '0');
+    }
+
+    return sum;
+}
+
+static List<string> ReadAndPrintOnConsole()
+{
+    var path = "info.txt";
+
+    var lines = new List<string>();
+
+    using (StreamReader sr = new StreamReader(path))
+    {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            lines.Add(line);
+        }
+    }
+
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+
+    return lines;
+}
---
+++ FilesN5/02/Program.cs
@@ -0,0 +1,44 @@
+﻿
+var path = "info.txt";
+var lines = new List<string>();
+using (StreamReader reader = new StreamReader(path))
+{
+    string line = reader.ReadLine();
+    lines.Add(line);
+    
+    string[] input = line.Split();
+    int a = int.Parse(input[0]);
+    int b = int.Parse(input[1]);
+
+    int[] digitCount = new int[10];
+
+    for (int i = a; i <= b; i++)
+    {
+        string numStr = i.ToString();
+       
+        foreach (char c in numStr)
+        {
+            int digit = c - '0';
+            digitCount[digit]++;
+        }
+    }
+
+    int maxCount = -1;
+    int digitWithMaxCount = -1;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (digitCount[i] > maxCount)
+        {
+            maxCount = digitCount[i];
+            digitWithMaxCount = i;
+        }
+
+        else if (digitCount[i] == maxCount && i < digitWithMaxCount)
+        {
+            digitWithMaxCount = i;
+        }
+    }
+
+    Console.WriteLine($"{digitWithMaxCount}: Digit {digitWithMaxCount} - {maxCount} times.");
+}
